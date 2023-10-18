c6c5122ce9ed8216b2a32d78a9e36b26451a642e
alp cetinkus <alpcetinkus@gmail.com>
2023-10-18 03:09:56 +0300

    Add BottomTabBar

---
+++ Movior/src/navigator/RootNavigator/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import { Entypo, FontAwesome5, Ionicons } from '@expo/vector-icons'; 
+import HomeNavigator from "../HomeNavigator"
+
+
+const Tab = createBottomTabNavigator()
+
+function index() {
+  return (
+    <Tab.Navigator 
+    initialRouteName='Home'
+    screenOptions={{
+        tabBarHideOnKeyboard:true,
+        tabBarShowLabel:false,
+        tabBarActiveTintColor: "#FF8F71",
+        tabBarInactiveTintColor: "#dadada",
+        headerShown:false,
+        tabBarStyle:{
+            height: 50,
+            backgroundColor:'#15141F',
+            borderTopWidth:1,
+            borderTopColor: '#15141F'
+        }
+    }}
+    >
+
+    <Tab.Screen 
+    name= "Home"
+    component={HomeNavigator}
+    options={{
+        tabBarIcon: ({color}) => (
+            <Entypo name="home" size={24} color={color} />
+        )
+    }}
+    />
+     <Tab.Screen 
+    name= "Discovery"
+    component={HomeNavigator}
+    options={{
+        tabBarIcon: ({color}) => (
+            <FontAwesome5 name="play" size={30} color={color} />
+            
+        )
+    }}
+    />
+
+<Tab.Screen 
+    name= "Profile"
+    component={HomeNavigator}
+    options={{
+        tabBarIcon: ({color}) => (
+            <Ionicons name="person" size={24} color={color} />
+        )
+    }}
+    />
+
+
+    </Tab.Navigator>
+  )
+}
+
+export default index
